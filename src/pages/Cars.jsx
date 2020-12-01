00cf614c01140ea9ecbb8dd764c1a7f4a7b7d50f
Nadim Salloum <hello@salloum.ch>
2020-12-01 11:27:00 +0100

    fix autoscout link

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -5,12 +5,20 @@ class Cars extends Component {
     super(props);
     this.state = {
       url: 'https://www.autoscout24.ch/de/hci/list?design=3825&filter=8685',
+      scriptUrl: 'https://www.autoscout24.ch/MVC/Content/as24-hci-desktop/js/e.min.js'
     };
+  }
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = this.state.scriptUrl;
+    this.div.appendChild(script);
   }
 
   render() {
     return (
-      <div className="page cars-page">
+      <div className="page page-cars" ref={el => (this.div = el)}>
         <div id="page-cars" class="anchor"></div>
         <div class="content-wrapper">
             <h2>Unser Autoangebot</h2>
