b38b5816a44680c8de88fa1cd13679d55eb9fbcd
Nadim Salloum <github@salloum.pm>
2026-02-16 21:02:37 +0100

    change footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer>
             <div className="content-wrapper">
-                <p>(2020) Design & Entwicklung von <a href="https://github.com/NateMS">Nadim Salloum</a></p>
+                <p>({new Date().getFullYear()}) Design & Entwicklung von <a href="https://github.com/NateMS">Nadim Salloum</a></p>
             </div>
         </footer>
     );
